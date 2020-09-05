3b1acd55a02fda467f7869d4732ca3f6940ae905
Nate <nathan.roberts.15@cnu.edu>
2020-09-05 11:53:43 -0700

    first commit

---
+++ generator.py
@@ -0,0 +1,7 @@
+"""
+Tool created to help user generate strong passwords
+"""
+
+#TODO: Create variables to store Alphabet in Upper and Lower case, numbers, and special characters
+#TODO: Let the user decide the strength of the password (i.e length, alphanumberic, specialcharacters or not)
+#TODO: Create variable to hold the combination of letters and numbers
