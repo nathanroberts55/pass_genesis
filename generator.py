f98f0bfac28424dfc01db0c77c3d61f9f13bf392
Nate <nathan.roberts.15@cnu.edu>
2020-09-05 18:00:17 -0700

    reworked the code to break down into functions. Added the option to use letters or not. working on the documentation

--- generator.py
+++ generator.py
@@ -1,20 +1,25 @@
 """
 Tool created to help user generate strong passwords
 """
-from random import *
+import random
 import string
 
 #TODO: Create variables to store Alphabet in Upper and Lower case, numbers, and special characters
-letters = string.ascii_letters
-numbers = string.digits
-special_chars = '!@#$%&*?+'
+LETTERS = string.ascii_letters
+NUMBERS = string.digits
+SPECIAL_CHARS = '!@#$%&*?+'
 
       
 #TODO: Let the user decide the strength of the password (i.e length, alphanumberic, special characters or not)
 #TODO: Create variable to hold the combination of letters and numbers
-def get_passwprd_length():
-        length = str(input('How long would should the password be?: ')).strip(" ")
-        return int(length)
+def get_password_length():
+    """Function to get the desired password length from the user
+
+    Returns:
+        int : Desired length of the password
+    """
+    length = str(input('How long would should the password be?: ')).strip(" ")
+    return int(length)
 
 def password_combination_choice():
     want_letters = str(input('Would you like to use letters? (True pr False): '))
@@ -32,23 +37,63 @@ def password_combination_choice():
         print('Invalid Values will generate a password with all available options')
 
         return [True, True, True]
-  
-def generate_password():
-  
 
-    if (alphanumberic == "y" and use_special_chrs == "y"): 
-        print('Printing Alphanumberic and Special Character Password\n')
-        password = "".join(choice(letters + numbers + special_chars) for x in range(length))
-    elif (use_special_chrs == "y"): 
-        password = "".join(choice(letters + special_chars) for x in range(length))
-        print('Printing Special Character Password\n')
-    elif (alphanumberic == "y"): 
-        print('Printing Alphanumberic Password\n')
-        password = "".join(choice(letters + numbers) for x in range(length))
-    else: 
-        print('Printing Password\n')
-        password = "".join(choice(letters) for x in range(length))
-    
+def get_string_constants(choice_list):
+    string_constant = ''
+    string_constant += LETTERS if choice_list[0] else ''
+    string_constant += NUMBERS if choice_list[1] else ''
+    string_constant += SPECIAL_CHARS if choice_list[2] else ''
+
+    return string_constant
+  
+def password_generator(cbl, length=8):
+    printable = get_string_constants(cbl)
+
+    printable = list(printable)
+    random.shuffle(printable)
+
+    random_password = random.choices(printable, k=length)
+    random_password = ''.join(random_password)
+
+    return random_password
+
+
+
+if __name__ == '__main__':
+    length = get_password_length()
+    choice_list = password_combination_choice()
+    password = password_generator(choice_list, length)
     print(password)
 
-generate_password()
+# # testing password generator with it's default length of 8
+# password_one = password_generator()
+
+# # testing password generator using user's input as length
+# password_length = get_password_length()
+# password_two = password_generator(password_length)
+
+# print("password one (" + str(len(password_one)) + "):\t\t" + password_one )
+# print("password one (" + str(len(password_two)) + "):\t\t" + password_two )
+
+
+
+
+# def generate_password():
+  
+
+#     if (alphanumberic == "y" and use_special_chrs == "y"): 
+#         print('Printing Alphanumberic and Special Character Password\n')
+#         password = "".join(choice(letters + numbers + special_chars) for x in range(length))
+#     elif (use_special_chrs == "y"): 
+#         password = "".join(choice(letters + special_chars) for x in range(length))
+#         print('Printing Special Character Password\n')
+#     elif (alphanumberic == "y"): 
+#         print('Printing Alphanumberic Password\n')
+#         password = "".join(choice(letters + numbers) for x in range(length))
+#     else: 
+#         print('Printing Password\n')
+#         password = "".join(choice(letters) for x in range(length))
+    
+#     print(password)
+
+# generate_password()
