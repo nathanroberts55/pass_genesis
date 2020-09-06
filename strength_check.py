e45ed64f673eb1674ab6813e6705bb0abf9c6d52
Nate <nathan.roberts.15@cnu.edu>
2020-09-06 16:51:43 -0700

    Able to create the password disctionary and return how many times a password was found in the pwnedpassword listings. Next step would be to break script into functions and allow for user input

--- strength_check.py
+++ strength_check.py
@@ -3,18 +3,18 @@ from requests.exceptions import HTTPError
 import hashlib
 
 """
-hashlib Documentation               : https: //docs.python.org/2/library/hashlib.html
-request.Reponse Object Documentation: https: //www.w3schools.com/python/ref_requests_response.asp
+hashlib Documentation               : https://docs.python.org/2/library/hashlib.html
+request.Reponse Object Documentation: https://www.w3schools.com/python/ref_requests_response.asp
 Reponse JSON parsing Pynative       : https://pynative.com/parse-json-response-using-python-requests-library/
 """
 
-password_test = 'password123'.encode()
-# password_test = password_test
-print(password_test)
+password_test = 'e3$52bHg2!'
+encoded_password_test = password_test.encode()
+# print(password_test)
 
-password_hash = hashlib.sha1(password_test).hexdigest()
+password_hash = hashlib.sha1(encoded_password_test).hexdigest()
 hash_first_five = password_hash[:5]
-rest_of_hash = password_hash[5:]
+rest_of_hash = password_hash[5:].upper()
 # print(password_hash)
 
 url = f"https://api.pwnedpasswords.com/range/{hash_first_five}"
@@ -32,11 +32,26 @@ try:
     response.raise_for_status()
     # access JSOn content
     text_response = response.text
-    print("Entire Text Format")
-    print(text_response)
+    lines = text_response.split("\n")
+    password_dict = {}
+
+    for line in lines:
+        password, times_seen = line.split(":")
+        password_dict[password.strip()] = times_seen.strip()
+    
+    times_found = password_dict.get(rest_of_hash)
+
+    if times_found:
+        print(f"Your password {password_test} was found {times_found} times in our list")
+        print("Please highly consider changing your password to something more secure")
+    else: 
+        print("Your password was not found on any of our list and is realitvely safe for now. Great Job!")
+    
+    # print(password_dict)
+    # print(text_response)
 
 except HTTPError as http_err: 
     print(f'HTTP error occurred: {http_err}')
-except Exception as err: 
-    print(f'Other error occurred: {err}')
+# except Exception as err: 
+#     print(f'Other error occurred: {err}')
     
