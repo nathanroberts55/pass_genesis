497aa20cb472546eaf74b65d2ba0a3729b206314
Nate <nathan.roberts.15@cnu.edu>
2020-09-06 17:28:08 -0700

    Refactored the code, added documentation/comments. Currently working, needs to be tested for invalid user input.

--- strength_check.py
+++ strength_check.py
@@ -8,50 +8,78 @@ request.Reponse Object Documentation: https://www.w3schools.com/python/ref_reque
 Reponse JSON parsing Pynative       : https://pynative.com/parse-json-response-using-python-requests-library/
 """
 
-password_test = 'e3$52bHg2!'
-encoded_password_test = password_test.encode()
-# print(password_test)
 
-password_hash = hashlib.sha1(encoded_password_test).hexdigest()
-hash_first_five = password_hash[:5]
-rest_of_hash = password_hash[5:].upper()
-# print(password_hash)
+def get_password():
+    """Gets the user's password they would like to check in the most common passwords list
 
-url = f"https://api.pwnedpasswords.com/range/{hash_first_five}"
+    Returns:
+        string: password they would like to check
+    """
 
-headers = {
-    'x-rapidapi-host': "moocher-io-common-passwords-v1.p.rapidapi.com",
-    'x-rapidapi-key': "e219e80c46mshe9f0d1499fa225cp106a16jsn9a2e0122d341"
-    }
+    user_password = str(input('Please enter the password you would like to check: ')).strip()
+    return user_password
 
-# response = requests.request("GET", url)
-# print(response.text)
 
-try: 
-    response = requests.get(url)
-    response.raise_for_status()
-    # access JSOn content
-    text_response = response.text
-    lines = text_response.split("\n")
-    password_dict = {}
+def hash_password(password):
+    """Hashes the password using SHA1 to prepare the password for interaction with the pwnedpasswords API
 
-    for line in lines:
-        password, times_seen = line.split(":")
-        password_dict[password.strip()] = times_seen.strip()
-    
-    times_found = password_dict.get(rest_of_hash)
+    Args:
+        password (string): password string recieved from get_password()
 
-    if times_found:
-        print(f"Your password {password_test} was found {times_found} times in our list")
-        print("Please highly consider changing your password to something more secure")
-    else: 
-        print("Your password was not found on any of our list and is realitvely safe for now. Great Job!")
-    
-    # print(password_dict)
-    # print(text_response)
+    Returns:
+        list: list contains the password as a string, the first 5 characters of the hashed password to be used in the url for the APi, and the remainder 
+        of the hashed password that is used to search the commonly used password list returned by the API.
+    """
 
-except HTTPError as http_err: 
-    print(f'HTTP error occurred: {http_err}')
-# except Exception as err: 
-#     print(f'Other error occurred: {err}')
-    
+    password_to_hash = password
+    encoded_password = password_to_hash.encode()
+    password_hash    = hashlib.sha1(encoded_password).hexdigest()
+    hash_first_five  = password_hash[:5]
+    rest_of_hash     = password_hash[5:].upper()
+
+    return [password_to_hash, hash_first_five, rest_of_hash]
+
+
+def check_common_passlist(hashlist):
+    """Uses the pwnedpasswords API to discover if the user's suggested password is found on commonly used passwords list
+
+    Args:
+        hashlist (list): list contains the password as a string, the first 5 characters of the hashed password to be used in the url for the APi, and the remainder 
+        of the hashed password that is used to search the commonly used password list returned by the API. 
+    """
+
+    # API url only uses the first 5 characters to find the password
+    url = f"https://api.pwnedpasswords.com/range/{hashlist[1]}"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        # API returns a list of passwords that share the first 5 hash values and the amount of times that they are found
+        # Examples can found at https://www.troyhunt.com/ive-just-launched-pwned-passwords-version-2/
+        text_response = response.text
+        lines         = text_response.split("\n")
+        password_dict = {}
+
+        for line in lines:
+            password     , times_seen       = line.split(":")
+            password_dict[password.strip()] = times_seen.strip()
+
+        times_found = password_dict.get(hashlist[2])
+
+        if times_found:
+            print(f"Your password {hashlist[0]} was found {times_found} times in our list")
+            print("Please highly consider changing your password to something more secure")
+        else:
+            print("Your password was not found on any of our list and is realitvely safe for now. Great Job!")
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+
+if __name__ == '__main__':
+    password        = get_password()
+    hashed_password = hash_password(password)
+    check_common_passlist(hashed_password)
