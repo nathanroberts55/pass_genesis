b9c1d9743e997961238827dfeb8547193729557d
Nate <nathan.roberts.15@cnu.edu>
2020-10-17 17:39:07 -0700

    used mock.patch to simulate user input without the need for manually putting the passwords to test in manually

--- test_common_pass.py
+++ test_common_pass.py
@@ -1,36 +1,40 @@
 import unittest
 import hashlib
+from unittest.mock import patch
 from common_pass import get_password, hash_password, check_common_passlist
 
-    """
-        Unit Testing Documentation (Real Python): https://realpython.com/python-testing/#writing-your-first-test
-        Unit Test Module Documentation          : https://docs.python.org/3/library/unittest.html
-    """
+"""
+    Unit Testing Documentation (Real Python): https://realpython.com/python-testing/#writing-your-first-test
+    Unit Test Module Documentation          : https://docs.python.org/3/library/unittest.html
+"""
+
+
 class TestCommonPass(unittest.TestCase):
-    def test_get_password(self):
-        """
+    """
         Test that checks that results of passing the valid argument "password123" to the method
-        """
-
-        test_pass = "password123"
-        result    = get_password()
-
-        self.assertEqual(result, test_pass)
-    
+    Args:
+        test_get_password ([string]): simulating the raw input of entering "password123"
+    """
+    @patch('builtins.input', return_value="password123")
+    def test_get_password(self, input):
+        self.assertEqual(get_password(), "password123")
+        
+    """
+    Test to see if the password is hashed properly
+    """
     def test_hash_password(self):
-        """
-        Test to see if the password is hashed properly
-        """
+        
 
         test_password = "password123"
 
         encoded_password = test_password.encode()
-        hashed_password  = hashlib.sha1(encoded_password).hexdigest()
-        hash_first_five  = hashed_password[:5]
-        rest_of_hash     = hashed_password[5:].upper()
-        result           = [test_password, hash_first_five, rest_of_hash]
+        hashed_password = hashlib.sha1(encoded_password).hexdigest()
+        hash_first_five = hashed_password[:5]
+        rest_of_hash = hashed_password[5:].upper()
+        result = [test_password, hash_first_five, rest_of_hash]
 
         self.assertEqual(hash_password(test_password), result)
+
 
 if __name__ == '__main__':
     unittest.main()
